d5b17dc5a18d9d48eaf292adf001e0f4b62637b5
Jason Davis <jason.davis@stelligent.com>
2020-12-11 07:41:35 -0700

    prompt for mfa and added aws configure set commands

--- 00-dev-environment/get-session-set-awsconfig.sh
+++ 00-dev-environment/get-session-set-awsconfig.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-creds_json=$(aws sts get-session-token  --serial-number  arn:aws:iam::324320755747:mfa/jason.davis.labs --token-code $1)
-export AWS_ACCESS_KEY_ID=$(echo "$creds_json" | jq .Credentials.AccessKeyId |tr -d '"')
-export AWS_SECRET_ACCESS_KEY=$(echo "$creds_json" | jq .Credentials.SecretAccessKey| tr -d '"')
-export AWS_SESSION_TOKEN=$(echo "$creds_json" | jq .Credentials.SessionToken|tr -d '"')
-export SERVICE_REGION=us-east-1
+set -e
+while true; do
+  read -p "MFA token:" mfa_token
+  case $mfa_token in
+    [0-9][0-9][0-9][0-9][0-9][0-9] ) creds_json=$(aws sts get-session-token  --serial-number  arn:aws:iam::324320755747:mfa/jason.davis.labs --token-code $mfa_token); break;;
+    * ) echo "Please enter 6-digit mfa token." ;;
+  esac
+done
+
+aws configure set aws_access_key_id $(echo "$creds_json" | jq .Credentials.AccessKeyId |tr -d '"') --profile temp
+aws configure set aws_secret_access_key $(echo "$creds_json" | jq .Credentials.SecretAccessKey| tr -d '"') --profile temp
+aws configure set aws_session_token $(echo "$creds_json" | jq .Credentials.SessionToken|tr -d '"') --profile temp
