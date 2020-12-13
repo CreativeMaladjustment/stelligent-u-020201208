955acac84050a2115cc11861fac966f84972ed88
CreativeMaladjustment <jason.davis@stelligent.com>
2020-12-13 14:44:46 +0000

    switched to using method that grabs version 1 by default

--- 01-cloudformation/lab1.3.2.py
+++ 01-cloudformation/lab1.3.2.py
@@ -3,38 +3,24 @@ import boto3
 import json
 from botocore.exceptions import ClientError
 
-def create_bucket(bucket_name, region=None):
-    """Create an S3 bucket in a specified region
+def check_status( cf_client, stack_name ):
+    lo_stacks = cf_client.describe_stacks(StackName=stack_name)["Stacks"]
+    # la_stack = lo_stacks[0]
+    # lc_cur_status = la_stack["StackStatus"]
+    # print("Current status of stack " + la_stack["StackName"] + ": " + lc_cur_status)
+    print("Current status of stack " + lo_stacks[0]["StackName"] + ": " + lo_stacks[0]["StackStatus"])
 
-    If a region is not specified, the bucket is created in the S3 default
-    region (us-east-1).
+    return lo_stacks[0]["StackStatus"]
 
-    :param bucket_name: Bucket to create
-    :param region: String region to create bucket in, e.g., 'us-west-2'
-    :return: True if bucket created, else False
-    """
 
-    # Create bucket
-    try:
-        if region is None:
-            s3_client = boto3.client('s3')
-            s3_client.create_bucket(Bucket=bucket_name)
-        else:
-            s3_client = boto3.client('s3', region_name=region)
-            location = {'LocationConstraint': region}
-            s3_client.create_bucket(Bucket=bucket_name,
-                                    CreateBucketConfiguration=location)
-    except ClientError as e:
-        logging.error(e)
-        return False
-    return True
-
-client= boto3.client("sts")
+client_sts= boto3.client("sts")
 
 with open('lab1.3.1.regionlist.json') as data_file:    
     data = json.load(data_file)
+
 for region in data['region']:
-    bucket_name = region + "-" + client.get_caller_identity()["Account"] + "-fiendly-name-jmd-020201212"
+    bucket_name = region + "-" + client_sts.get_caller_identity()["Account"] + "-fiendly-name-jmd-020201212"
     print (bucket_name)
-    create_bucket(bucket_name, region=None)
-    
+    # create_bucket(bucket_name, region=None)
+    cf_client = boto3.client('cloudformation', region)
+    check_status( cf_client, stack_name="jmd-020201211-008" )    
