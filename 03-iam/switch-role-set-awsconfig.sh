68a877507f268ecf5532fc79d7b8b29531b42cc8
CreativeMaladjustment <jason.davis@stelligent.com>
2020-12-15 18:36:47 +0000

    syntax change for not being in a while statement

--- 03-iam/switch-role-set-awsconfig.sh
+++ 03-iam/switch-role-set-awsconfig.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-creds_json=$(aws sts assume-role --role-arn arn:aws:iam::324320755747:role/su-jdlabs-321 --role-session-name lab321 --profile temp); break;;
+creds_json=$(aws sts assume-role --role-arn arn:aws:iam::324320755747:role/su-jdlabs-321 --role-session-name lab321 --profile temp)
 
 aws configure set aws_access_key_id $(echo "$creds_json" | jq .Credentials.AccessKeyId |tr -d '"') --profile temprole
 aws configure set aws_secret_access_key $(echo "$creds_json" | jq .Credentials.SecretAccessKey| tr -d '"') --profile temprole
