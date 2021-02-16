b58b364a1fc744069842a6eae284da08b923f8cd
jason.davis <jason.davis@stelligent.com>
2021-02-16 23:15:24 +0000

    try except block on externalExecutionSummary which seems to be commit comment

--- 12-codepipeline/craw-pipeline.py
+++ 12-codepipeline/craw-pipeline.py
@@ -27,6 +27,18 @@ def l_p():
               print (i["name"], "id:", i_lpe["pipelineExecutionId"], i_lpe["status"])
             #   print (i_lpe)
     #   logger.debug(response)
+          response_lae = codepipeline.list_action_executions(pipelineName=i["name"],maxResults=99)
+        #   print (response)
+          for i_lae in response_lae["actionExecutionDetails"]:
+            #   print (i_lpe["name"])
+              print (i["name"], "action id:", i_lae["pipelineExecutionId"], i_lae["actionExecutionId"], i_lae["status"])
+#              print (i_lae)
+              #, i_lae["status"], i_lae["output"]["executionResult"]["externalExecutionSummary"])
+              try:
+                  print ("rawr", i_lae["output"]["executionResult"]["externalExecutionSummary"])
+              except:
+                  print ("no summary")
+
   except Exception as error:
       logger.exception(error)
     #   logger.debug(response)
