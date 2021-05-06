1c5af7283311b18caaaccf496d6d7cc3b5f24b7e
EC2 Default User <ec2-user@ip-172-31-30-131.ec2.internal>
2021-05-06 01:31:55 +0000

    starting on writing the poc cdk code

--- cdk/aws-image-builder-poc/aws_image_builder_poc/aws_image_builder_poc_stack.py
+++ cdk/aws-image-builder-poc/aws_image_builder_poc/aws_image_builder_poc_stack.py
@@ -1,15 +1,16 @@
-from aws_cdk import core as cdk
+from aws_cdk import (
+    aws_iam as iam,
+    aws_imagebuilder as imagebuilder,
+    aws_ec2 as ec2,
+    aws_ssm as ssm,
+    core
+)
 
-# For consistency with other languages, `cdk` is the preferred import name for
-# the CDK's core module.  The following line also imports it as `core` for use
-# with examples from the CDK Developer's Guide, which are in the process of
-# being updated to use `cdk`.  You may delete this import if you don't need it.
-from aws_cdk import core
-
+from datetime import datetime
 
 class AwsImageBuilderPocStack(cdk.Stack):
 
     def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        poc_component = imagebuilder.CfnComponent(self, "poc_component", name="poc_component", platform="Linux", version="1.0.0", )
