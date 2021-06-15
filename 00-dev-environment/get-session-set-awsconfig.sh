965ad97b40e61745c85561287ff9c48bad8f5706
FloutEnvy <jd@nonviolence.us>
2021-06-15 10:01:40 -0600

    nope needed SHELL

--- 00-dev-environment/get-session-set-awsconfig.sh
+++ 00-dev-environment/get-session-set-awsconfig.sh
@@ -11,3 +11,4 @@ done
 aws configure set aws_access_key_id $(echo "$creds_json" | jq .Credentials.AccessKeyId |tr -d '"') --profile temp
 aws configure set aws_secret_access_key $(echo "$creds_json" | jq .Credentials.SecretAccessKey| tr -d '"') --profile temp
 aws configure set aws_session_token $(echo "$creds_json" | jq .Credentials.SessionToken|tr -d '"') --profile temp
+#SHELL
