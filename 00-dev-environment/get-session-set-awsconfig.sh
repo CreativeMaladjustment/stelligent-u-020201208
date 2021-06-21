1b719f59606e723efaa444fbea03f3d522bdd040
Jason Davis <jason.davis@stelligent.com>
2021-06-21 12:43:28 -0600

    changing set

--- 00-dev-environment/get-session-set-awsconfig.sh
+++ 00-dev-environment/get-session-set-awsconfig.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 set -e
-while true; do
-  read -p "MFA token:" mfa_token
+exec 3<&0
+while read -r "MFA token:" mfa_token; do 
   case $mfa_token in
     [0-9][0-9][0-9][0-9][0-9][0-9] ) creds_json=$(aws sts get-session-token  --serial-number  arn:aws:iam::324320755747:mfa/jason.davis.labs --token-code $mfa_token); break;;
     * ) echo "Please enter 6-digit mfa token." ;;
   esac
 done
+exec 3<&-
 
 aws configure set aws_access_key_id $(echo "$creds_json" | jq .Credentials.AccessKeyId |tr -d '"') --profile temp
 aws configure set aws_secret_access_key $(echo "$creds_json" | jq .Credentials.SecretAccessKey| tr -d '"') --profile temp
